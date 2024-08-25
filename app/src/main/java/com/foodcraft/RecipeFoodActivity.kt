b9db1cf86e6c2107702e625618cb4a26e34632fc
Miguel Kawakami <migskawakami@gmail.com>
2024-08-25 15:43:00 -0300

    fix recipe food activity

--- app/src/main/java/com/foodcraft/RecipeFoodActivity.kt
+++ app/src/main/java/com/foodcraft/RecipeFoodActivity.kt
@@ -2,61 +2,41 @@ package com.foodcraft
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.foodcraft.viewmodel.UserViewModel
 
 class RecipeFoodActivity : AppCompatActivity() {
 
-    private val userViewModel by viewModels<UserViewModel>()
-<<<<<<< HEAD
-    private lateinit var btnBack: AppCompatImageButton // Corrigido
+    private lateinit var btnBack: AppCompatImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_food)
+
         setBtnBack()
-=======
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recipe_food)
-
->>>>>>> stash
-
+        setBtnNewUser()
     }
 
+     private fun setBtnBack() {
+         btnBack = findViewById(R.id.backButton)
+         btnBack.setOnClickListener {
+             val intent = Intent(
+                 this@RecipeFoodActivity,
+                 PrincipalActivity::class.java
+             )
+             startActivity(intent)
+         }
+     }
 
-<<<<<<< HEAD
-    private fun setBtnBack() {
-        btnBack  = findViewById(R.id.backButton)
-        btnBack.setOnClickListener(View.OnClickListener {
-            val intent = Intent(
-                this@RecipeFoodActivity,
-                PrincipalActivity::class.java
-=======
-    private fun setBtnNewUser() {
+     private fun setBtnNewUser() {
         val btnNewUser = findViewById<TextView>(R.id.textView3)
-        btnNewUser.setOnClickListener(View.OnClickListener {
-            val intent = Intent(
-                this@RecipeFoodActivity,
-                RegisterActivity::class.java
->>>>>>> stash
-            )
-            startActivity(intent)
-
-        })
-    }
-
-
-
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return true
+         btnNewUser.setOnClickListener {
+             val intent = Intent(
+                 this@RecipeFoodActivity,
+                 RegisterActivity::class.java
+             )
+             startActivity(intent)
+         }
     }
 }
